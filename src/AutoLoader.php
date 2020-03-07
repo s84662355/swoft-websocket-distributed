951e8652bc27c8630ad1c5c9a866275e16408ca7
陈嘉豪 <271375247@qq.com>
2020-03-07 10:26:25 +0800

    a

---
+++ src/AutoLoader.php
@@ -0,0 +1,51 @@
+<?php declare(strict_types=1);
+
+
+namespace cjhswoftWebSocket;
+
+
+use Swoft\Bean\Annotation\Mapping\Bean;
+use ReflectionException;
+use Swoft\Bean\Exception\ContainerException;
+use Swoft\SwoftComponent;
+
+/**
+ * Class AutoLoader
+ *
+ * @since 2.0
+ */
+class AutoLoader extends SwoftComponent
+{
+    /**
+     * @return array
+     */
+    public function getPrefixDirs(): array
+    {
+        return [
+            __NAMESPACE__ => __DIR__,
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function metadata(): array
+    {
+        return [];
+    }
+
+    /**
+     * @return array
+     * @throws ReflectionException
+     * @throws ContainerException
+     */
+    public function beans(): array
+    {
+        
+        return [
+ 
+  
+        ];
+
+    }
+}
